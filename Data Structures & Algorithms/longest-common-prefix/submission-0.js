0526266b42c4ef06367b4fe7a1d3e387a17302d6
loogitakurrey <loogitakurrey38@gmail.com>
2026-04-21 09:22:46 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let prefix = "";
+
+        for(let i = 0; i < strs[0].length; i++){
+            for(let s of strs){
+                if(i== s.length || s[i] !== strs[0][i]){
+                    return s.slice(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
